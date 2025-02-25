76ea6bdd2e81a3b48fa779034a6f89287dcaf7a0
oldq2333 <ruiqixu@usc.edu>
2025-02-25 02:56:54 -0800

    Three new towers

---
+++ Assets/Scripts/IceProjectile.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceProjectile : Projectile
+{
+    public float slowAmount = 0.5f;    
+    public float slowDuration = 3f;   
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Enemy normalEnemy = other.GetComponent<Enemy>();
+            RangedEnemy rangedEnemy = other.GetComponent<RangedEnemy>();
+
+            if (normalEnemy != null)
+            {
+                StartCoroutine(ApplySlowEffect(normalEnemy));
+            }
+            else if (rangedEnemy != null)
+            {
+                StartCoroutine(ApplySlowEffect(rangedEnemy));
+            }
+        }
+    }
+
+    IEnumerator ApplySlowEffect(MonoBehaviour enemy)
+    {
+        var speedField = enemy.GetType().GetField("speed");
+        float originalSpeed = (float)speedField.GetValue(enemy);
+        speedField.SetValue(enemy, originalSpeed * slowAmount);
+
+        yield return new WaitForSeconds(slowDuration);
+
+        if (enemy != null)
+        {
+            speedField.SetValue(enemy, originalSpeed);
+        }
+    }
+}
