2fdb475891d41c6d2923addcba79c3bf002c0ef7
Linda Tam <lindatam@usc.edu>
2025-03-09 22:16:53 -0700

    Fix spawner UI hover layering bug

---
+++ Assets/OverlayUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlayUI : MonoBehaviour
+{
+    public static OverlayUI instance;
+
+    private void Awake()
+    {
+        // Check if instance already exists
+        if (instance == null)
+        {
+            // If not, set instance to this
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            // If instance already exists and it's not this, then destroy this to enforce the singleton.
+            Destroy(gameObject);
+        }
+        
+        // Set this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+}
