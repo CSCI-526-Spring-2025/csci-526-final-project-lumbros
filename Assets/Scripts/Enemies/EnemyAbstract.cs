182dc0018064726b726ae02d1fca1fe93fa429d7
Linda Tam <lindatam@usc.edu>
2025-03-10 12:35:20 -0700

    Fixed Build Settings to build Alpha

--- Assets/Scripts/Enemies/EnemyAbstract.cs
+++ Assets/Scripts/Enemies/EnemyAbstract.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using UnityEditor.Experimental.GraphView;
 
 public abstract class EnemyAbstract : MonoBehaviour, IDamageable
 {
