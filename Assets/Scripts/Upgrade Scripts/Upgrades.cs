8f8d2f8a4fc6c36b3e72348e8c3804bf6f1eeb26
CJH <2737212633@qq.com>
2025-03-13 14:28:32 -0700

    upgrade3

--- Assets/Scripts/Upgrade Scripts/Upgrades.cs
+++ Assets/Scripts/Upgrade Scripts/Upgrades.cs
@@ -19,7 +19,10 @@ public class Upgrades : MonoBehaviour
     private System.Action selectedUpgrade;
     private string selectedUpgradeName;
     private int selectedUpgradeCost;
-
+    public int towerHP = 0;
+    public int towerAutoHeal = 0;
+    public int towerDamage = 0;
+    public int towerRange = 0;
     private Dictionary<string, List<(string, System.Action, int)>> heroUpgrades = new Dictionary<string, List<(string, System.Action, int)>>();
     private Dictionary<string, List<(string, System.Action, int)>> towerUpgrades = new Dictionary<string, List<(string, System.Action, int)>>();
 
@@ -30,25 +33,128 @@ public class Upgrades : MonoBehaviour
         hero = GameObject.FindGameObjectWithTag("Player");
         manager = GameObject.FindGameObjectWithTag("Manager");
 
-        AddUpgrade(heroUpgrades, "Hero Damage", "+1 to Hero Damage", () => hero.GetComponent<AutoAttack>().damage += 1, 50);
-        AddUpgrade(heroUpgrades, "Hero Damage", "+2 to Hero Damage", () => hero.GetComponent<AutoAttack>().damage += 2, 100);
-        AddUpgrade(heroUpgrades, "Attack Speed", "Increase Hero Attack Speed (0.7x)", () => hero.GetComponent<AutoAttack>().attackCooldown *= 0.7f, 75);
-        AddUpgrade(heroUpgrades, "Hero HP", "+1 to Hero HP", () => hero.GetComponent<Health>().maxHealth++, 60);
-        AddUpgrade(heroUpgrades, "Hero Auto Heal", "+1 to Hero Auto Heal", () => hero.GetComponent<Health>().autoHeal += 1, 80);
-        AddUpgrade(heroUpgrades, "Move Speed", "Increase Move Speed +1", () => hero.GetComponent<HeroMovement>().moveSpeed += 1, 70);
+        // Hero Upgrades
+        AddUpgrade(heroUpgrades, "Hero Damage", "+1 to Hero Damage", () => hero.GetComponent<AutoAttack>().damage += 1,3);
+        AddUpgrade(heroUpgrades, "Hero Damage", "+2 to Hero Damage", () => hero.GetComponent<AutoAttack>().damage += 2,4);
 
-        AddUpgrade(towerUpgrades, "Tower Range", "+1 to Tower Attack Range", () => ApplyTowerUpgrade(tower => tower.GetComponent<AutoAttack>().attackRange += 1), 90);
-        AddUpgrade(towerUpgrades, "Tower Auto Heal", "+1 to Tower Auto Heal", () => ApplyTowerUpgrade(tower => tower.GetComponent<Health>().autoHeal += 1), 85);
-        AddUpgrade(towerUpgrades, "Tower Damage", "+1 to Tower Damage", () => ApplyTowerUpgrade(tower => tower.GetComponent<AutoAttack>().damage += 1), 110);
-        AddUpgrade(towerUpgrades, "Tower HP", "+1 to Tower HP", () => ApplyTowerUpgrade(tower => tower.GetComponent<Health>().maxHealth++), 95);
+        AddUpgrade(heroUpgrades, "Attack Speed", "Increase Hero Attack Speed (0.7x)", () => hero.GetComponent<AutoAttack>().attackCooldown *= 0.7f,5);
+        AddUpgrade(heroUpgrades, "Attack Speed", "Increase Hero Attack Speed (0.5x)", () => hero.GetComponent<AutoAttack>().attackCooldown *= 0.4f,10);
 
+        AddUpgrade(heroUpgrades, "Hero HP", "+2 to Hero HP", () =>
+        {
+            Health hp = hero.GetComponent<Health>();
+            hp.maxHealth +=2;
+            hp.currentHealth +=2;
+        },2);
+        AddUpgrade(heroUpgrades, "Hero HP", "+4 to Hero HP", () =>
+        {
+            Health hp = hero.GetComponent<Health>();
+            hp.maxHealth += 4;
+            hp.currentHealth += 4;
+        },4);
+        AddUpgrade(heroUpgrades, "Hero Auto Heal", "+1 to Hero Auto Heal", () => hero.GetComponent<Health>().autoHeal += 1,2);
+        AddUpgrade(heroUpgrades, "Hero Auto Heal", "+2 to Hero Auto Heal", () => hero.GetComponent<Health>().autoHeal += 2,4);
+        AddUpgrade(heroUpgrades, "Move Speed", "Increase Move Speed +1", () => hero.GetComponent<HeroMovement>().moveSpeed += 1,3);
+        AddUpgrade(heroUpgrades, "Move Speed", "Increase Move Speed +2", () => hero.GetComponent<HeroMovement>().moveSpeed += 2,4);
+        AddUpgrade(heroUpgrades,"Hero Bounce", "Hero Projectiles Bounce +1", () => hero.GetComponent<AutoAttack>().heroBounces += 1,3);
+        AddUpgrade(heroUpgrades,"Hero Bounce", "Hero Projectiles Bounce +2", () => hero.GetComponent<AutoAttack>().heroBounces += 2,4);
+        // Tower Upgrades
+        // AddUpgrade(towerUpgrades, "Tower Count", "+1 to Max Tower Count", () => manager.GetComponent<CustomSceneManager>().maxTowerCount++);
+
+        AddUpgrade(towerUpgrades, "Tower Range", "+2 to Tower Attack Range", () =>
+        {
+            foreach (var tower in towers)
+            {
+                Debug.Log("In Tower Loop");
+                tower.GetComponent<AutoAttack>().attackRange += 2;
+            }
+            towerRange += 2; 
+        },3);
+        AddUpgrade(towerUpgrades, "Tower Range", "+3 to Tower Attack Range", () =>
+        {
+            foreach (var tower in towers)
+            {
+                Debug.Log("In Tower Loop");
+                tower.GetComponent<AutoAttack>().attackRange += 3;
+            }
+            towerRange += 3;    
+        },4);
+        AddUpgrade(towerUpgrades, "Tower Auto Heal", "+1 to Tower Auto Heal", () =>
+        {
+            foreach (var tower in towers)
+            {
+                tower.GetComponent<Health>().autoHeal += 1;
+            }
+            towerAutoHeal += 1;
+        },3);
+        AddUpgrade(towerUpgrades, "Tower Auto Heal", "+2 to Tower Auto Heal", () =>
+        {
+            foreach (var tower in towers)
+            {
+                tower.GetComponent<Health>().autoHeal += 2;
+            }
+            towerAutoHeal += 2;
+        },5);
+        AddUpgrade(towerUpgrades, "Tower Damage", "+1 to Tower Damage", () =>
+        {
+            foreach (var tower in towers)
+            {
+                Debug.Log("In Tower Loop");
+                tower.GetComponent<AutoAttack>().damage += 1;
+            }
+            towerDamage += 1;
+        },3);
+        AddUpgrade(towerUpgrades, "Tower Damage", "+2 to Tower Damage", () =>
+        {
+            foreach (var tower in towers)
+            {
+                Debug.Log("In Tower Loop");
+                tower.GetComponent<AutoAttack>().damage += 2;
+            }
+            towerDamage += 2;
+                
+        },5);
+
+        AddUpgrade(towerUpgrades, "Tower HP", "+2 to Tower HP", () =>
+        {
+            foreach (var tower in towers)
+            {
+                Health hp = tower.GetComponent<Health>();
+                hp.maxHealth +=2;
+                hp.currentHealth+=2;
+                 
+            }
+            towerHP += 2;
+        },3);
+        AddUpgrade(towerUpgrades, "Tower HP", "+4 to Tower HP", () =>
+        {
+            foreach (var tower in towers)
+            {
+                Health hp = tower.GetComponent<Health>();
+                hp.maxHealth += 4;
+                hp.currentHealth += 4;
+                
+            }
+            towerHP += 4;
+        },4);
         AssignUpgrades(heroUpgrades);
+    }
+    public void reset(){
+        towerHP = 0;
+        towerAutoHeal = 0;
+        towerDamage = 0;
+        towerRange = 0;
     }
 
     void Update()
     {
         towers = GameObject.FindGameObjectsWithTag("Tower");
         currWave = WaveManager.Instance.currentWave;
+
+        if(currWave == 2){
+            reset();
+        }
+        
         Wave.text = "Wave " + (currWave - 1) + " Completed!";
 
         UpgradeText.text = (phase == 1) ? "Choose an upgrade for hero" : "Choose an upgrade for all towers";
