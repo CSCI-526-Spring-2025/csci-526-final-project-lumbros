de75cd47464b2561a058c98e87089ce62069898d
Linda Tam <lindatam@usc.edu>
2025-03-26 16:59:08 -0700

    Moved UI Around

--- Assets/Scripts/UI/HeroTextUI.cs
+++ Assets/Scripts/UI/HeroTextUI.cs
@@ -7,6 +7,7 @@ public class HeroTextUI : MonoBehaviour
     private TMP_Text textComponent;
     private GameObject HeroObject;
     private AutoAttack attackInfo;
+    private HeroMovement movementInfo; 
     private Health health;
     // Start is called before the first frame update
     void Start()
@@ -15,11 +16,12 @@ public class HeroTextUI : MonoBehaviour
         HeroObject = GameObject.FindGameObjectWithTag("Player");
         health =  HeroObject.GetComponent<Health>();
         attackInfo = HeroObject.GetComponent<AutoAttack>();
+        movementInfo = HeroObject.GetComponent<HeroMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        textComponent.text = $"Hero\nHealth: {health.currentHealth} / {health.maxHealth} \nAttack Damage: {attackInfo.attackRange} \nAttack Speed: {attackInfo.attackCooldown}";
+        textComponent.text = $"Hero\nHealth: {health.currentHealth} / {health.maxHealth} \nAttack Damage: {attackInfo.attackRange} \nAttack Speed: {attackInfo.attackCooldown} \nMovement Speed: {movementInfo.moveSpeed}";
     }
 }
