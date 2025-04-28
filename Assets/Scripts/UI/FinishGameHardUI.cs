7418a6593b4d79227cc65294a9da00e7a96816ee
xlunardargoonx <phillipho1993@gmail.com>
2025-04-28 15:15:03 -0700

    Fixed bug in gameFinishUI
    -gamefinishUI gets recreated onLoad

--- Assets/Scripts/UI/FinishGameHardUI.cs
+++ Assets/Scripts/UI/FinishGameHardUI.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FinishGameUI : MonoBehaviour
+public class FinishGameHardUI : MonoBehaviour
 {
     // Static reference to the instance of our GameOverUI
-    public static FinishGameUI instance;
+    public static FinishGameHardUI instance;
 
     private void Awake()
     {
